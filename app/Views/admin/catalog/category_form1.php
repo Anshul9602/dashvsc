192d0053bed25353d78a86a4f01d051a180b4f84
Anshul9602 <anshulkumar242507@gmail.com>
2025-02-14 11:52:56 +0530

    updated code

--- app/Views/admin/catalog/category_form1.php
+++ app/Views/admin/catalog/category_form1.php
@@ -9,13 +9,10 @@
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Sheets</a></li>
                         <li class="breadcrumb-item active"><a href="javascript:void(0)">Sheets</a></li>
-                       
-
                     </ol>
                 </div>
             </div>
             <div class="col-sm-6 justify-content-sm-end mt-2 mt-sm-0 d-flex">
-               
                     <button class="btn btn-primary btn-rounded"id="save_btn33">
                         Save
                     </button>
@@ -23,7 +20,6 @@
                 <button class="btn btn-danger btn-rounded">
                     <li class="fa fa-arrow-left"></li>
                 </button>
-
             </div>
         </div>
         <!-- row -->
@@ -37,7 +33,6 @@
                                     <li class="nav-item">
                                         <a class="nav-link active" data-toggle="tab" href="#home1"><i class="la la-home mr-2"></i> General</a>
                                     </li>
-                                   
                                 </ul>
                                 <div class="tab-content">
                                    <div class="tab-pane fade show active" id="home1" role="tabpanel">
@@ -89,10 +84,6 @@
                                                 </select>
                                             </div>
                                         </div>
-                                       
-                                      
-                                       
-
                                         <button type="submit" id="s_btnn5" class="btn btn-primary d-none">Submit</button>
                                       </div>
                                 </div>
@@ -108,8 +99,6 @@
 <script>
       document.getElementById('save_btn33').addEventListener('click', function() {
       // Trigger a click on the second button
-
-     
       document.getElementById('s_btnn5').click();
    });
     // Attach a click event to the first button
@@ -125,7 +114,6 @@
             des.classList.remove("show");
             des.classList.remove("active");
         }
-
         // Add the 'active' class to the 'cat-menu' item
         const catMenu = document.getElementById("cat-menu");
         const cat = document.getElementById("forms");
